d79b481236da957f15d8710dad139cbaaa3a7380
root <cheerlasiva143@gmail.com>
2023-02-06 09:00:32 +0000

    insert

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import UserInfo from '../UserInfo'
 import './index.css'
 
 const Home = () => (
-  <div className="home-container">
+  <div className="home-container" data-testid="loader">
     <UserInfo />
     <BlogList />
   </div>
